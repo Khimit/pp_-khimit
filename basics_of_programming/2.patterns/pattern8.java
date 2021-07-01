a67286c61a579fc5469722839347578e91c87078
Khimit <46605529+Khimit@users.noreply.github.com>
2021-07-01 18:54:49 +0530

    Create pattern8.java

---
+++ basics_of_programming/2.patterns/pattern8.java
@@ -0,0 +1,35 @@
+/*
+
+				*	
+			*		
+		*			
+	*				
+*
+
+*/
+
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+
+    // write ur code here
+    int n = scn.nextInt();
+
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= n; j++) {
+        if (j == n - i + 1) {       // condition from observation
+          System.out.print("*\t");
+        }
+        else {
+          System.out.print("\t");
+        }
+      }
+      System.out.println();
+
+    }
+
+  }
+}
